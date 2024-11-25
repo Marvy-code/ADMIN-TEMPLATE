63e5d5d667a1654f6398b5e42454e2e23c765c58
Marvy-code <gwlad.marvy@gmail.com>
2024-11-25 19:17:54 +0100

    visualisation du modal pour les détails de la mission

--- app/(rh)/missions/ListMissions.tsx
+++ app/(rh)/missions/ListMissions.tsx
@@ -19,8 +19,17 @@ const Liste = () => {
     const { user } = useUser();
 
     const [data, setData] = useState([]); // Stocke les données récupérées
-    const [loading, setLoading] = useState(true); // Gestion du chargement
-    const [error, setError] = useState(null); // Gestion des erreurs
+    const [modalOpen, setModalOpen] = useState(false)
+    const [selectedRow, setSelectedRow] = useState({
+        Continent_mission: "",
+        Date_creation: "",
+        Description_mission: "",
+        Id_mission: "",
+        Objet_mission: "",
+        Pays_mission: "",
+        Ville_mission: "",
+        Cree_par: ""
+    })
 
     const columns = [
         {
@@ -44,7 +53,7 @@ const Liste = () => {
             name: "Action",
             cell:(row:any)=>(
                 <div>
-            <button type='button' className="bg-blue-400 text-white p-2 rounded-full mr-1" onClick={() => handleAction(row.Id_mission)}><LiaEyeSlashSolid /></button>
+            <button type='button' className="bg-blue-400 text-white p-2 rounded-full mr-1" onClick={() => handleAction(row)}><LiaEyeSlashSolid /></button>
             <button type='button' className="bg-yellow-300 text-white p-2 rounded-full mr-1"><HiMiniPencilSquare /></button>
             <button type='button' className="bg-red-950 text-white p-2 rounded-full"><IoTrashBin /></button>
         </div>
@@ -65,23 +74,34 @@ const Liste = () => {
     }
 
     const fetchData = async () =>{
-        setLoading(true);
-
         try{
             await axiosInstance.get('mission/getall')
             .then(res=>{
                 setData(res.data)
-
-                console.log(res.data)
             })
         }catch(err){
 
         }
     }
 
-    const handleAction = (id:any) => {
-        alert(`Action pour la ligne avec ID : ${id}`);
-      };
+    const handleAction = (row:any) => {
+        setSelectedRow(row)
+        setModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setSelectedRow({
+            Continent_mission: "",
+            Date_creation: "",
+            Description_mission: "",
+            Id_mission: "",
+            Objet_mission: "",
+            Pays_mission: "",
+            Ville_mission: "",
+            Cree_par: ""
+        });
+    }
 
     useEffect(()=>{
         fetchData()
@@ -103,8 +123,50 @@ const Liste = () => {
                         data={data} 
                         customStyles={customStyles}
                         pagination
+                        highlightOnHover
                     />
                 </div>
+
+                {modalOpen && (
+                    <div className='fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center'>
+                        <div
+                            className={`bg-white p-6 rounded-lg shadow-lg w-full lg:w-2/3 transform transition-transform duration-300 ease-in-out ${
+                            modalOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                            }`}
+                            onClick={(e) => e.stopPropagation()} // Empêcher la propagation pour ne pas fermer le modal en cliquant à l'intérieur
+                        >
+                            {selectedRow && (
+                                <div>
+                                    <div className="flex justify-between bg-gray-300 items-center px-6 py-4 border-b">
+                                        <h3 className='uppercase'>Détails de la mission: <span className='font-bold text-green-700'>{selectedRow.Objet_mission}</span></h3>
+                                    </div>
+
+                                    <div className="p-6">
+                                        <div className="text-gray-700 flex items-center w-full md:flex-row flex-col">
+                                            <div className="w-full md:w-1/3"><p>Continent: <strong>{selectedRow.Continent_mission}</strong></p></div>
+                                            <div className="w-full md:w-1/3"><p>Pays: <strong>{selectedRow.Pays_mission}</strong></p></div>
+                                            <div className="w-full md:w-1/3"><p>Ville: <strong>{selectedRow.Ville_mission}</strong></p></div>
+                                        </div>
+                                        <p className='mt-4 justify-normal text-gray-700'>Description: {selectedRow.Description_mission}</p>
+
+                                        <p className='mt-10 justify-normal text-gray-400 float-righ'>
+                                            <span className='float-end'>Cette mission a été créée le {selectedRow.Date_creation}, par {selectedRow.Cree_par}</span>
+                                        </p>
+                                    </div>
+
+                                    <div className="flex justify-end gap-2 px-6 py-4 border-t">
+                                        <button
+                                            onClick={closeModal}
+                                            className="mt-4 bg-[#B2241B] text-white px-4 py-2 rounded float-end"
+                                            >
+                                            Fermer
+                                        </button>
+                                    </div>
+                                </div>  
+                            )}
+                        </div>
+                    </div>
+                )}
                 <CreateMission />
             </div>
         </div>
